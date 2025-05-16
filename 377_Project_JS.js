ad32928c291c1d4dfe6c8fddbca8c4550850e10b
thenoah54 <thenoah54@gmail.com>
2025-05-16 15:43:16 -0400

    5/16

--- 377_Project_JS.js
+++ 377_Project_JS.js
@@ -1,20 +1,34 @@
-function getsteamData() {
-    const usingMock = true;
+var hostName = 'https://www.cheapshark.com/api/1.0/deals?storeID=1';
 
+function getsteamData() {
+    // const usingMock = false;
     // deals https://www.cheapshark.com/redirect?dealID={id}
-    const hostName = usingMock ? 
-        'https://mock-ea65f4fc2d424b3387f4ed888e171972.mock.insomnia.rest/api/1.0/deals' : 'placeholder';
+    // hostName = usingMock ? 
+    //     'https://mock-ea65f4fc2d424b3387f4ed888e171972.mock.insomnia.rest/api/1.0/deals' : 
+    //     'https://www.cheapshark.com/api/1.0/deals?storeID=1';
+
+    slider();
+
     fetch(hostName).then((result) => result.json()).then((resultJson) => {
-        console.log(resultJson);
+        // console.log(resultJson);
+        var num = 1
         resultJson.forEach((resultData) => {
-            console.log(`Name: ${resultData.title},
-                Rating: ${resultData.steamRatingPercent},
-                Deal Rating: ${resultData.dealRating},
-                Normal Price: ${resultData.normalPrice}
-                Sale Price: $${resultData.salePrice}`);
+            // console.log(`Name: ${resultData.title},
+            //     img: ${resultData.thumb},
+            //     Rating: ${resultData.steamRatingPercent},
+            //     Deal Rating: ${resultData.dealRating},
+            //     Normal Price: ${resultData.normalPrice}
+            //     Sale Price: $${resultData.salePrice}`);
 
             const tableRow = document.createElement('tr');
+            tableRow.className = 'steamRow'
+            
+            // num
+            const listOrder = document.createElement('td');
+            listOrder.innerHTML = num++;
+            listOrder.id = 'listOrder'
 
+            // title text
             // const steamName = document.createElement('td');
             // steamName.innerHTML = resultData.title;
 
@@ -45,6 +59,7 @@ function getsteamData() {
             });
             
             // adding to row
+            tableRow.appendChild(listOrder);
             tableRow.appendChild(steamName);
             tableRow.appendChild(steamRating);
             tableRow.appendChild(steamdealRating);
@@ -55,16 +70,52 @@ function getsteamData() {
             tableInfo.append(tableRow);
         });
     });
+}
+
+function searchGame(event) {
+    event.preventDefault();
 
     // games https://www.cheapshark.com/api/1.0/games?title={title}
-    const hostName2 = usingMock ? 
-        'https://mock-ea65f4fc2d424b3387f4ed888e171972.mock.insomnia.rest/api/1.0/games' : 'placeholder';
-    fetch(hostName2).then((result) => result.json()).then((resultJson) => {
-        console.log(resultJson);
-        resultJson.forEach((resultName) => {
-            // console.log(`Name: ${resultName.external}`)
-        });
-    });
+    const title = document.getElementById('searchBar').value;
+    console.log(`Searching for: ${title}`);
+
+    hostName = `https://www.cheapshark.com/api/1.0/deals?storeID=1&title=${title}`;
+
+    const tableInfo = document.getElementById('tableInfo');
+    tableInfo.innerHTML = `
+        <tr id="labels">
+            <th></th>
+                <th>Game</th>
+                <th>Rating</th>
+                <th>Deal Rating</th>
+                <th>Normal Price</th>
+                <th>Sale Price</th>
+        </tr>
+    `;
+
+    getsteamData(); 
+
 }
+
+function slider() {
+    const path = 'https://www.cheapshark.com/api/1.0/deals?storeID=1&AAA=1';
+    const slider = document.getElementById('gameSlider');
+    // Clear previous slider content
+    slider.innerHTML = '';
+    fetch(path).then((results) => results.json()).then(resultsJson => {
+        // console.log(resultsJson);
+        resultsJson.forEach((pics) => {
+            const slider = document.getElementById('gameSlider');
+            const newGame = document.createElement('img');
+            newGame.src = pics.thumb;
+            newGame.style.width = '100%';
+            newGame.style.height = '100%';
+            slider.append(newGame); 
+        });
+        simpleslider.getSlider();
+    });
+
+}
+
 
 window.onload = getsteamData;
